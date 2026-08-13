225304452415bdff8e300df3ae324bd0edc8238f
Daniel345772 <marroquindaniel632@gmail.com>
2026-08-12 18:05:11 -0600

    solución  ejercicio 5 de panel de control

---
+++ ejercico5.cs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("1. Consultar estado");
+        Console.WriteLine("2. Mostrar temperatura");
+        Console.WriteLine("3. Mostrar operadores");
+        Console.WriteLine("4. Reiniciar sistema");
+        Console.WriteLine("5. Salir");
+        Console.WriteLine("Seleccione una opcion:");
+
+        string opcion = Console.ReadLine()!;
+
+        switch (opcion)
+        {
+            case "1":
+                Console.WriteLine("Consultando el estado del sistema");
+                break;
+            case "2":
+                Console.WriteLine("Temperatura actual: Estable");
+                break;
+            case "3":
+                Console.WriteLine("Mostrando lista de operadores");
+                break;
+            case "4":
+                Console.WriteLine("Reiniciando el sistema");
+                break;
+            case "5":
+                Console.WriteLine("Saliendo del panel");
+                break;
+            default:
+                Console.WriteLine("OPCION NO VALIDA");
+                break;
+        }
+    }
+}
