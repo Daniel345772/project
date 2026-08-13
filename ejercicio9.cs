576e428db4155dbc292d6aeb30e8c2ffaa7ae9f8
Daniel345772 <marroquindaniel632@gmail.com>
2026-08-12 18:11:38 -0600

    ejericico 9

---
+++ ejercicio9.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] nombres = { "Ana", "Carlos", "Leo", "Roberto", "Luz", "Daniel" };
+
+        Console.WriteLine("Operadores con más de 4 caracteres:");
+        
+        foreach (string nombre in nombres)
+        {
+            if (nombre.Length > 4)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+    }
+}
