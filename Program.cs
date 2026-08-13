c806f922f6a666186f0e84daa128a78e6f2cb792
Nelsonso-17 <nelson.cuellar1@catolica.edu.sv>
2026-08-12 18:01:50 -0600

    Solucion reto 4: Clasificacion de nivel de alerta

--- Program.cs
+++ Program.cs
@@ -1,36 +1,45 @@
 ﻿using System;
 
-Console.Write("Ingrese el nivel de alerta (0-10): ");
-string entrada = Console.ReadLine();
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el nivel de alerta (0-10): ");
+            string entrada = Console.ReadLine();
 
-if (int.TryParse(entrada, out int nivel))
-{
-    if (nivel == 0)
-    {
-        Console.WriteLine("NORMAL");
+            if (int.TryParse(entrada, out int nivel))
+            {
+                if (nivel == 0)
+                {
+                    Console.WriteLine("NORMAL");
+                }
+                else if (nivel >= 1 && nivel <= 3)
+                {
+                    Console.WriteLine("ADVERTENCIA");
+                }
+                else if (nivel >= 4 && nivel <= 6)
+                {
+                    Console.WriteLine("PELIGRO");
+                }
+                else if (nivel >= 7 && nivel <= 9)
+                {
+                    Console.WriteLine("CRÍTICO");
+                }
+                else if (nivel == 10)
+                {
+                    Console.WriteLine("EMERGENCIA");
+                }
+                else
+                {
+                    Console.WriteLine("NIVEL DE ALERTA INVÁLIDO");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NIVEL DE ALERTA INVÁLIDO");
+            }
+        }
     }
-    else if (nivel >= 1 && nivel <= 3)
-    {
-        Console.WriteLine("ADVERTENCIA");
-    }
-    else if (nivel >= 4 && nivel <= 6)
-    {
-        Console.WriteLine("PELIGRO");
-    }
-    else if (nivel >= 7 && nivel <= 9)
-    {
-        Console.WriteLine("CRÍTICO");
-    }
-    else if (nivel == 10)
-    {
-        Console.WriteLine("EMERGENCIA");
-    }
-    else
-    {
-        Console.WriteLine("NIVEL DE ALERTA INVÁLIDO");
-    }
-}
-else
-{
-    Console.WriteLine("NIVEL DE ALERTA INVÁLIDO");
 }
