2766f635d473f060039b5979d92b8690c6014bd2
Daniel345772 <marroquindaniel632@gmail.com>
2026-08-12 18:12:54 -0600

    ejercicio 11

---
+++ ejercicio11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese su nombre completo:");
+        string nombreEntrada = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(nombreEntrada))
+        {
+            Console.WriteLine("Entrada inválida.");
+            return;
+        }
+
+        string nombreLimpio = nombreEntrada.Trim();
+
+        while (nombreLimpio.Contains("  "))
+        {
+            nombreLimpio = nombreLimpio.Replace("  ", " ");
+        }
+
+        Console.WriteLine("Nombre limpio: " + nombreLimpio);
+        Console.WriteLine("Cantidad de caracteres: " + nombreLimpio.Length);
+        Console.WriteLine("Mayúsculas: " + nombreLimpio.ToUpper());
+        Console.WriteLine("Minúsculas: " + nombreLimpio.ToLower());
+    }
+}
